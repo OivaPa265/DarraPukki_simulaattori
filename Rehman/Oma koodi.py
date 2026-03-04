1756d1603df6eea76dffbb2417f483a385116225
Rehmanae <rehman.ahmad2@metropolia.fi>
2026-03-04 10:16:26 +0200

    Add files via upload

---
+++ Rehman/Oma koodi.py
@@ -0,0 +1,29 @@
+#Yees löysit Baarista haalarimerkki.
+#Minä teen koodi ilmoitusviesti esim teen sellaista kun löydän haalarimerkkin
+#  niin siitä tule ilmoitus ja teen funktion joka pitää kirjaa
+#montako haalarimerkkiä on löydetty
+
+def haalarimerkki():
+    print("Haalarimerkki löydetty!")
+
+haalarimerkki()
+
+haalarimerkki=[]
+
+
+#Kysytään käyttäjältä kuinka monta haalarimerkki on löydetty?
+def haalarimerkki_määrä():
+    maara = input("Kuinka paljon Haalarimerkki on löydetty?:")
+    return haalarimerkki
+
+# haalarimerkki = input("Montako haalarimerkki on nyt?:")
+
+while haalarimerkki >=1:
+    haalarimerkki.append("maara")
+
+#while True:
+#    print("Haalarimerkkiä on löydetty:", maara)
+
+
+
+
