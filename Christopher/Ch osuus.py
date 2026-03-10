5ea3ef386537ec725e677c4d6ebc0d7ec12a534b
Christmu <Christopher.munday@metropolia.fi>
2026-03-10 12:18:18 +0200

    Tässä on itselleni esimerrki kirjoitettu että pääsen alkuun.

--- Christopher/Ch osuus.py
+++ Christopher/Ch osuus.py
@@ -1,86 +1,130 @@
-# kirjoita miten alkoholi on polttoiane ja sen miten pelin häviää jos vaikka alkoholi loppuu.
-# kirjoita myös mitä tapahtuu kun voitat eli keräät kaikki haalarimerkit.
-# 10 haalarimerkkiä ja kun voittaa saa erikois haalarimerkin
-from os.path import join
-
-# Alkoholi polttoaine ja miten häviää
-import mysql.connector
 import random
-yhteys = mysql.connector.connect(
-         host='127.0.0.1',
-         port= 3306,
-         database='flight_game',
-         user='root',
-         password='1507:Chris!05',
-         autocommit=True
-         )
+import sys
 
-# -- ohjelma alkaa tästä --
 
-def polttoaine_positiivinen():
-    print("Vrummm vrummm vrummm")
+# ------------------ Rehmanin osuus ohjelmasta alkaa tästä ---------------------
+
+
+# Tässä Funktio laskee kahden pisteen välisen etäisyyden
+
+def appropiste(piste1, piste2):
+    if piste1 > piste2:              # Tarkistaa kumpi pisteistä on suurempi
+        erotus = piste1 - piste2     # Laskee pisteiden välinen erotus
+    else:
+        erotus = piste2 - piste1     # Eli jos piste2 on suurempi niin lasketaan näin
+    return erotus   # Palauttaa lasketun etäisyyden
+
+
+#Arvotaan eka piste väliltä 1000-10000
+appropiste1 = random.randint(1000,10000)
+
+#Arvotaan toka piste väliltä 1000-1000
+appropiste2 = random.randint(1000,10000)
+
+#Tulostetaan pisteet
+print(appropiste1)
+print(appropiste2)
+
+# Kutsutaan funktiota ja tallenetaan tulosta muuttujaan, joka on matka.
+matka = appropiste(appropiste1, appropiste2)
+
+#Tulostetaan pisteiden  välinen etäisyys
+print("Erotus on", matka)
+
+
+
+# Teen koodi, kun sulla on 10 haalrimerrki, antaa erikois haalarinmerkkin
+
+# Esim print sulla on 10/10 tässä sulle erikoismerkki
+
+# Kuinka paljon alkoholi kuluu per metri, Alkoholi 1 Litra per Kilometri
+
+kulutus = 1000
+
+#Funktio laskee alkoholin kulutuksen
+def kulutus(matka):
+    litrat = matka/ 1000
+    return litrat
+
+tulos = kulutus(matka)
+
+
+print("Alkoholia kuluu", tulos,"litraa")
+
+
+
+# ----------------- Christopherin osuus ohjelmasta alkaa tästä ---------------------
+
+def pelaajan_liike():
+    alkoholi_ml = alkoholi * 1000
+    if alkoholi_ml > matka:
+        print("Vrummm vrummm vrummm lennetään seuraavalle pisteelle")
+    elif alkoholi_ml < matka:
+        print("Bisse loppu et voi enää liikkua joten hävisit!")
+        sys.exit()
     # pelaaja liikkuu jotenkin perkele
-    return polttoaine_positiivinen
+    return
 
-def polttoaine_negatiivinen():
-    #sql = "SELECT alcohol FROM game"
-
-
-    print("Hävisit pelin koska olet niin vitun paska juomaan!!! ")
-    # pelaaja vittu häviää koska se on paska
-    return polttoaine_negatiivinen
+#def polttoaine_negatiivinen():
+    print("===============================================")
+    print("Hävisit pelin koska olet niin huono juomaan!!! ")
+    print("===============================================")
+    return
 
 
-def polttoaine_liikaa():
-    print("Hävisit pelin koska olet vitun juoppo!!! ")
+#def polttoaine_liikaa():
+    if alkoholi > 13:
+        print("===================================")
+        print("Hävisit pelin koska olet juoppo!!! ")
+        print("===================================")
     # pelaaja taas vittu häivää koska se on vitun juoppo
-    return polttoaine_liikaa
+    return
 
 def voitto():
+    print("==================================================================")
     print("Jeee voitit sait kaikki haalarimerkit tässä on erikois apro merkki")
+    print("==================================================================")
     # Kun pelaaja kerää kaikki haalarimerkit se voittaa ja tähän tulee koodi voitto ruudulle
-    return voitto
+    return
 
 
 def lista_lukumaara():
-    perkele = len(haalarimerkit_lista)
-    if perkele == 10:
+    pituus = len(haalarimerkit_lista)
+    if pituus == 10:
         voitto()
-    return lista_lukumaara
-
+    return pituus
 
 
 # -- Pääohjelma --
 
 # alkoholin väliaikainen määrrittely
 # alkoholi = int(input("Montako alkoholi juomaa juot? (1-10) jos ylität 10 häviät pelin "))
-haalarimerkki = input("Keräätkö haalarimerkin? (joo tai ei) ")
+
 alkoholi = random.randint(0,10)
 haalarimerkit_lista = []
 
 
 
 while alkoholi > 0:
-    polttoaine_positiivinen()
-    if haalarimerkki == "joo":
+    print("=========================================")
+    haalarimerkki = input("Keräätkö haalarimerkin? (paina Enter) ")
+    print("- - - - - - - - - - - - - - - - - - - - -")
+    # polttoaine_positiivinen()
+    if haalarimerkki == "":
+        print("- - - - - - - - - - - - - - - - - - - - -")
+        print("Haalarimerkki löydetty!")
+        print("- - - - - - - - - - - - - - - - - - - - -")
         haalarimerkit_lista.append(haalarimerkki)
         print("Sinulla on haalarimerkkejä tällä hetkellä",len(haalarimerkit_lista))
-        lista_lukumaara()
-    elif haalarimerkki == "ei":
-        print("EI oo mahdollista vittu perkele vittu vittu DUADUSHADOIDHWAOIDJAOW")
-
-    alkoholi = random.randint(0,10)
-    print(f"Joit {alkoholi} juomaa! ")
-    if alkoholi < 1:
-        polttoaine_negatiivinen()
-    if alkoholi > 10:
-        polttoaine_liikaa()
-
-
-
-#int(input("Montako alkoholi juomaa juot? (1-10) jos ylität 10 häviät pelin "))
-    #if alkoholi < 1:
-        #polttoaine_negatiivinen()
-    #if alkoholi > 10:
-        #polttoaine_liikaa()
-    #haalarimerkki = input("Keräätkö haalarimerkin? (joo tai ei) ")
+        lkm = lista_lukumaara()
+        if lkm == 10:
+            break
+    print("=========================================")
+    print("=========================================")
+    juo = input("Kaadanko kurkustasi bisseä? (paina Enter)")
+    print("- - - - - - - - - - - - - - - - - - - - -")
+    if juo == "":
+        alkoholi = random.randint(0,26)
+        print("- - - - - - - - - - - - - - - - - - - - -")
+        print(f"Kaadoin kurkustasi alas {alkoholi} juomaa! ")
+        pelaajan_liike()
